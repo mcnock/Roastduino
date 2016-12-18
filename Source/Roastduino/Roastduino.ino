2a3233a8d3ea5673beb95b02d3bfbf9880736ea8
Andrew Nock <ajnock@gmail.com>
2016-12-18 08:36:12 -0800

    Made include local

--- Source/Roastduino/Roastduino.ino
+++ Source/Roastduino/Roastduino.ino
@@ -1,15 +1,15 @@
-#include <Time.h>
-#include <TimeLib.h>
+#include "libraries\Time-master\Time.h"
+#include "libraries\Time-master\TimeLib.h"
 
-#include <Adafruit_TFTLCD.h>
-#include <TouchScreen.h>
-#include <max6675.h>
-#include <Adafruit_GFX.h>
-#include <gfxfont.h>
-#include <SPI.h>
+#include "libraries\TFTLCD-Library-master\Adafruit_TFTLCD.h"
+#include "libraries\Touch-Screen-Library-master\TouchScreen.h"
+#include "libraries\max6675.h"
+#include "libraries\Adafruit-GFX-Library-master\Adafruit_GFX.h"
+#include "libraries\Adafruit-GFX-Library-master\gfxfont.h"
+//#include <SPI.h>
 #include <EEPROM.h>
 #include <Wire.h>
-#include "Average.h"
+#include "libraries\Average.h"
 #include "Chrono.h"
 
 // ===========
